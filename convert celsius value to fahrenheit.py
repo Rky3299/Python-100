c4cdc07924e5a088f7ece1413290d902f0c6698f
Rky3299 <155969456+Rky3299@users.noreply.github.com>
2024-07-30 12:09:09 +0530

    Add files via upload

---
+++ convert celsius value to fahrenheit.py
@@ -0,0 +1,3 @@
+A=float(input("enter celcius: "))
+fehra=(A*9/5)+32
+print("Converted celcius in fehranit: ",fehra)
---
+++ sum of 3 digits.py
@@ -0,0 +1,5 @@
+a = int(input("Enter the first digit: "))
+b = int(input("Enter the second digit: "))
+c = int(input("Enter the third digit: "))
+
+print(a+b+c)
