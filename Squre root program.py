2d666995c1e4c240ebde76ca704c1a0c1379d496
Rky3299 <155969456+Rky3299@users.noreply.github.com>
2024-08-10 00:03:58 +0530

    Add files via upload

---
+++ Squre root program.py
@@ -0,0 +1,3 @@
+num = int(input("enter number: "))
+sr = num**(1/2)
+print("the squre root of the given num9ber is ",sr)w
---
+++ convert kilomeater to miles.py
@@ -0,0 +1,3 @@
+km = float(input("enter km: "))
+miles = km*0.62137
+print(km,"in miles:",miles)
